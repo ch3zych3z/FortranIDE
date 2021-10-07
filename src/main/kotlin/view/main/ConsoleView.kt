ca7f84b89881176642241726ebc896ae1aba9ac3
ViktorKarasss <70607580+ViktorKarasss@users.noreply.github.com>
2021-10-07 23:56:37 +0300

    Added views for console and top menu

--- src/main/kotlin/view/main/ConsoleView.kt
+++ src/main/kotlin/view/main/ConsoleView.kt
@@ -1,9 +1,15 @@
 package view.main
 
 import javafx.scene.Node
+import javafx.scene.control.TextArea
 
 class ConsoleView {
-    fun setupConsoleView(): Node {
-        TODO()
+
+
+    fun setupConsoleView(): TextArea {
+        val console = TextArea()
+        console.setMaxSize(10000000.0,100.0)
+        return console
     }
+
 }
